3d51eb59df25ae928fa599e4ee10cad25f1d094d
Martin Borgman <borgman.martin@gmail.com>
2026-01-05 20:21:44 +0100

    Initial commit

---
+++ src/test/java/dev/martinborgman/otel/OtelApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.martinborgman.otel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

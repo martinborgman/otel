fbeeba83c4af9e49de9a5a9f0d19868b9f1b5067
Martin <borgman.martin@gmail.com>
2026-06-26 16:34:34 +0200

    added wall and alloc to the profiling data

--- src/main/java/dev/martinborgman/otel/Application.java
+++ src/main/java/dev/martinborgman/otel/Application.java
@@ -3,6 +3,7 @@ package dev.martinborgman.otel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.pyroscope.http.Format;
 import io.pyroscope.javaagent.PyroscopeAgent;
 import io.pyroscope.javaagent.config.Config;
 
@@ -14,6 +15,12 @@ public class Application {
             new Config.Builder()
                 .setApplicationName("my-spring-app")
                 .setServerAddress("http://localhost:4040")
+				.setFormat(Format.JFR)
+				.setProfilingEvent(io.pyroscope.javaagent.EventType.CPU)
+ 				.setProfilingEvent(io.pyroscope.javaagent.EventType.WALL)
+ 				.setProfilingEvent(io.pyroscope.javaagent.EventType.ALLOC)
+				.setProfilingAlloc("512k")
+        		.setProfilingLock("10ms")
                 .build()
         );
 

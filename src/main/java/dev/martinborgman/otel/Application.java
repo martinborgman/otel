d67a8954c5b154b6b4d06f4eb412b2b2e2d53ebd
Martin <borgman.martin@gmail.com>
2026-06-26 15:39:50 +0200

    Fixed profiling. Aparently profiling is not part of Spring OTEL

--- src/main/java/dev/martinborgman/otel/Application.java
+++ src/main/java/dev/martinborgman/otel/Application.java
@@ -3,11 +3,22 @@ package dev.martinborgman.otel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.pyroscope.javaagent.PyroscopeAgent;
+import io.pyroscope.javaagent.config.Config;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		PyroscopeAgent.start(
+            new Config.Builder()
+                .setApplicationName("my-spring-app")
+                .setServerAddress("http://localhost:4040")
+                .build()
+        );
+
 		SpringApplication.run(Application.class, args);
 	}
-
 }
+
+
